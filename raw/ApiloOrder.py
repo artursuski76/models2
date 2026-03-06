9029fa3f5de73c72cf0c978b1aaca9c269ef6905
artursuski <suski.artur@gmail.com>
2026-03-06 07:25:35 +0100

    v1.0.73

---
+++ raw/ApiloOrder.py
@@ -0,0 +1,54 @@
+from datetime import date
+from typing import Optional, List, Any
+
+from pydantic import Field
+from pydantic import ConfigDict, Field
+
+from models2.abase import BasicBasic
+from models2.helpers.WooOrdersHelpers import WooBillingAndShipping, WooLineItems, WooShippingLines, WooMetaData
+
+
+
+class ApiloOrder(BasicBasic):
+    model_config = ConfigDict(extra='allow')
+
+    model_name: str = Field(
+        "ApiloOrder",
+        title="Nazwa Modelu",
+        json_schema_extra={"exclude_from_form": True}
+    )
+
+    my_id: str = Field(
+        title="Unikalny ID, np.: Abcde:PL789331 *",
+        pattern=r"^[A-Za-z0-9]+(:[A-Z0-9][A-Za-z0-9]*)*$",
+        description="Dozwolone tylko litery A-Z, a-z, cyfry 0-9 oraz myślniki. Zalecamy tax-id, pesel, ewentualnie nr-telefonu lub nazwę. Podana treść będzie głównym indeksem wyszukiwania klienta. Podanie istniejącego identyfikatora nadpisze dane w kartotece."
+    )
+
+    processed_at: Optional[date] = Field(
+        None,
+        title="Data przetworzenia zamówienia",
+        json_schema_extra={"exclude_from_form": True}
+    )
+    inv_gen_at: Optional[date] = Field(
+        None,
+        title="Data przetworzenia zamówienia",
+        json_schema_extra={"exclude_from_form": True}
+    )
+
+    class FormConfig:
+        page_title = "Zlecenia WooCommerce"
+        header = "Lista zleceń pobranych z WooCommerce"
+        # Optymalizacja N1QL: pobieraj tylko te pola do listy
+        list_view_fields = [
+            "id", "number", "status", "total", "currency",
+            "date_created", "date_completed", "billing", "shipping", "payment_method_title",
+            "last_sync", "my_id"
+        ]
+        default_sort_field = "date_completed"
+        default_sort_dir = "DESC"
+
+    class Couchbase:
+        bucket = "Accounting"
+        scope = "raw"
+        collection = "apilo_order"
+
