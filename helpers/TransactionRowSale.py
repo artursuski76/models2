2a07eb3ddae0ff528e3dc92ce9bd30f96312bc5e
artursuski <suski.artur@gmail.com>
2026-03-07 17:11:07 +0100

    v1.0.91

--- helpers/TransactionRowSale.py
+++ helpers/TransactionRowSale.py
@@ -17,7 +17,7 @@ class TransactionRowBaseSale(BaseModel):
     quantity: float = Field(title="Ilość")
     # uom: InventoryItemUnit = Field(title="Jednostka miary")
     unit_price_net: Decimal = Field(max_digits=12, decimal_places=2, title="Cena jednostkowa netto")
-    name: str = Field(title="Nazwa")
+    name: Optional[str] = Field(None, title="Nazwa")
 
     amount_net: Decimal = Field(..., max_digits=12, decimal_places=2, title="Netto")
     amount_vat: Decimal = Field(..., max_digits=12, decimal_places=2, title="VAT")
