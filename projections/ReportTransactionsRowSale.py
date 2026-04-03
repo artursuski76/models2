ecd288c0dc674aa486de6175993bb6629db7fbf2
artursuski <suski.artur@gmail.com>
2026-04-03 14:07:19 +0200

    duzy fix 2

--- projections/ReportTransactionsRowSale.py
+++ projections/ReportTransactionsRowSale.py
@@ -9,34 +9,48 @@ from models2.abase import BasicBasic
 
 
 class ReportTransactionsRowSale(BasicBasic):
-    cost_inv_my_id: str
-    cost_inv_source: str
-    cost_inv_inv_nr: str
-    cost_inv_counterparty_id: str
-    cost_inv_currency: str
-    cost_inv_date_posting: date
-    cost_inv_ksef_ref: Optional[str]
-    cost_inv_original_payload_ref: str
-
-
-    row_type: Optional[str] = Field()
-    amount_net: Decimal = Field(..., max_digits=12, decimal_places=2, title="Netto")
-    amount_vat: Decimal = Field(..., max_digits=12, decimal_places=2, title="VAT")
-    amount_gross: Decimal = Field(..., max_digits=12, decimal_places=2, title="Brutto")
-
-    vat_rate_doc: int = Field(default=0, title="VAT_doc %")
-    vat_rate_jpk: Optional[int] = Field(
-        default=23,
-        title="VAT_jpk %"
+    model_name: str = Field(
+        "ReportTransactionsRowSale",
+        title="Nazwa Modelu",
+        json_schema_extra={"exclude_from_form": True}
     )
+    sale_inv_my_id: str
+    sale_inv_status: str
+    sale_inv_source: str
+    sale_inv_counter: int
+    sale_inv_date_posting: date
+    sale_inv_date_sale: Optional[date]
+    sale_inv_counterparty_id: str
+    sale_inv_currency: str
+    sale_inv_ksef_ref: Optional[str]
+    sale_inv_original_payload_ref: Optional[str] = None
+
+    sale_inv_cash_method: bool = False
+    sale_inv_self_billing: bool = False
+    sale_inv_reverse_charge: bool = False
+    sale_inv_split_payment: bool = False
+    sale_inv_ec_simplified: bool = False
+
+    vat_category: Optional[str] = None
+    tu: Optional[str] = None
     inventory_item_id: Optional[str] = Field(
         default=None,
         title="Kod inwent."
     )
+    quantity: Optional[Decimal] = None
+    uom: Optional[str] = None
+    unit_price_net: Optional[Decimal] = None
     description: Optional[str] = Field(
         None,
         title="Opis"
     )
+    amount_net: Decimal = Field(..., max_digits=12, decimal_places=2, title="Netto")
+    amount_vat: Decimal = Field(..., max_digits=12, decimal_places=2, title="VAT")
+    amount_gross: Decimal = Field(..., max_digits=12, decimal_places=2, title="Brutto")
+
+    vat_rate_doc: Optional[float] = Field(default=0, title="VAT_doc %")
+    vat_rate_oss: Optional[float] = Field(default=0, title="VAT_oss %")
+    ctry_oss: Optional[str] = None
 
     class Couchbase:
         bucket = "Accounting"
